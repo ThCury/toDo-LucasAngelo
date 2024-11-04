a7c058ec6d3226d067bf4916ebea3ca46a039381
PortoPedro <pedro20049955080800@gmail.com>
2024-11-04 12:18:17 -0300

    java code for exception handler

---
+++ src/main/java/com/pedroporto/todosimple/services/exceptions/DataBindingViolationException.java
@@ -0,0 +1,12 @@
+package com.pedroporto.todosimple.services.exceptions;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class DataBindingViolationException extends DataIntegrityViolationException {
+    public DataBindingViolationException(String message) {
+        super(message);
+    }
+}
